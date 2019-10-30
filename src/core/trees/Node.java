18347c51091d455afabd95ad339f67d5a0ce4fed
Redcxx <weilue.luo@student.manchester.ac.uk>
2019-10-30 12:31:53 +0000

    Revert "hashcode for symbol"
    This reverts commit 89c6a7e9493afaf35c5e4a535a230aa267443443.

---
+++ src/core/trees/Node.java
@@ -0,0 +1,24 @@
+package core.trees;
+
+import core.symbols.Symbol;
+import org.jetbrains.annotations.Contract;
+
+public abstract class Node implements NodeInsertion {
+    public Symbol value;
+
+    @Contract(pure = true)
+    protected Node() {
+        this(null);
+    }
+
+    @Contract(pure = true)
+    protected Node(Symbol v) {
+        value = v;
+    }
+
+    protected String getSpaces(int depth) {
+        return "-".repeat(depth * 3);
+    }
+
+    public abstract String toString(int depth);
+}
